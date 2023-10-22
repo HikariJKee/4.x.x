1c9f3f202bff36eeb877290e4209075007063dd2
Андрей Медведев <hikarijkee@gmail.com>
2023-10-22 22:03:48 +0500

    4.1.17

---
+++ 4.x.x/Program.cs
@@ -0,0 +1,30 @@
+﻿class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+        var Color = Console.ReadLine();
+        if (Color == "gray")
+        {
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Your color is gray!");
+        }
+        else if (Color == "green")
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+
+            Console.WriteLine("Your color is green!");
+        }
+        else
+        {
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Your color is cyan!");
+        }
+    }
+}
